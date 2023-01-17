396bd24366c2194ce9ee2a819e0bad379a98c3f6
OrGaNiK777 <OrGaNiK777@yandex.ru>
2023-01-18 00:57:24 +0500

    17.01.2023 dom

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-
 let popup = document.querySelector(".popup");
 let profileEditButton = document.querySelector(".profile__edit-button");
 let popupButtonSave = document.querySelector(".popup__button-save");
 let popupButtonClose = document.querySelector(".popup__button-close");
+let popupName = document.querySelector(".popup__name");
+let popupAbout = document.querySelector(".popup__about");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+
 
 function popupOpened() {
 	popup.classList.add("popup__opened");
+	popupName.value = profileName.textContent;
+	popupAbout.value = profileAbout.textContent;
 }
-profileEditButton.addEventListener("click", popupOpened);
 
 function popupClose() {
 	popup.classList.remove("popup__opened");
 }
+
+profileEditButton.addEventListener("click", popupOpened);
 popupButtonClose.addEventListener("click", popupClose);
-popupButtonSave.addEventListener("click", popupClose);
+// popupButtonSave.addEventListener("click", popupClose);
 
-
-
-
-let profileInfo = document.querySelector('.profile__info')
-
-let name = document.querySelector(".popup__name");
-let about = document.querySelector(".popup__about");
-
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
+function handleFormSubmit(evt) {
+	evt.preventDefault();
 }
+	profileName.textContent = (`${popupName}`)
+	profileAbout.textContent = (`${123}`)
 
+
+
+popup.addEventListener('submit', handleFormSubmit); 
