d9037437cbafbcfc37ff3eac6490e79a175c7d06
OrGaNiK777 <OrGaNiK777@yandex.ru>
2023-01-18 22:23:08 +0500

    18.01.2023 dom

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
 let popup = document.querySelector(".popup");
 let profileEditButton = document.querySelector(".profile__edit-button");
-let popupButtonSave = document.querySelector(".popup__button-save");
+// let popupButtonSave = document.querySelector(".popup__button-save");
 let popupButtonClose = document.querySelector(".popup__button-close");
-let popupName = document.querySelector(".popup__name");
-let popupAbout = document.querySelector(".popup__about");
+let popupName = document.getElementById("popupName");
+let popupAbout = document.getElementById("popupAbout");
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about");
 
-const popupOpened = () => {
-	popup.classList.add("popup__opened");
+const openedPopup = () => {
+	//открытие попап
+	popup.classList.add("popup_opened");
 	popupName.value = profileName.textContent;
 	popupAbout.value = profileAbout.textContent;
 };
 
-const popupClose = () => {
-	popup.classList.remove("popup__opened");
+const closePopup = () => {
+	//закрытие попап
+	popup.classList.remove("popup_opened");
 };
 
-const popupSave = () => {
-	popup.classList.remove("popup__opened");
+const savePopup = () => {
+	popup.classList.remove("popup_opened");
 	profileName.textContent = `${popupName.value}`;
 	profileAbout.textContent = `${popupAbout.value}`;
 };
 
-profileEditButton.addEventListener("click", popupOpened);
-popupButtonClose.addEventListener("click", popupClose);
-popupButtonSave.addEventListener("click", popupSave);
+profileEditButton.addEventListener("click", openedPopup);
+popupButtonClose.addEventListener("click", closePopup);
+popupButtonSave.addEventListener("click", savePopup);
 
 function handleFormSubmit(evt) {
 	evt.preventDefault();
 }
-
-popup.addEventListener("submit", handleFormSubmit);
